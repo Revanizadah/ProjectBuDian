1c69e1f8b9f0136b49b39006a6a1679aa9865ce7
Revaniza <113415844+Revanizadah@users.noreply.github.com>
2023-04-04 13:52:29 +0700

    Add files via upload

---
+++ controller/koneksi.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Noval
+ */
+public class koneksi {
+    static Connection kon;  
+    
+    public static Connection connection() {
+        if (kon == null) {
+            MysqlDataSource data = new MysqlDataSource();
+            data.setDatabaseName("toko");
+            data.setUser("root");
+            data.setPassword("");
+            try {
+                kon = data.getConnection();
+                System.out.println("Sudah Terkoneksi");
+            }catch (Exception e) {
+                System.out.println("Belum Terkoneksi");
+            }
+        }
+        return kon;
+    }
+}
